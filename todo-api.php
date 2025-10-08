92b1aea8f8faadd586b03db5c2ca0a1dbab961e8
Marco Scheidhuber <github@mail.j23d.de>
2025-10-08 11:19:11 +0000

    Update deleting of a todo to use SQL.

--- todo-api.php
+++ todo-api.php
@@ -88,14 +88,11 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case 'DELETE':
         // Get data from the input stream.
         $data = json_decode(file_get_contents('php://input'), true);
-        // Filter Todo to delete from the list.
-        $todos = array_values(
-            array_filter($todos,
-                function($todo) use ($data) {
-                    return $todo['id'] !== $data['id'];
-        }));
-        // Write the Todos back to JSON file.
-        file_put_contents('todo.json', json_encode($todos));
+
+        // Delete the todo from our database.
+        $statement = $pdo->prepare("DELETE FROM todo WHERE id=:id");
+        $statement->execute(['id' => $data['id']]);
+
         // Tell the client the success of the operation.
         echo json_encode(['status' => 'success']);
         write_log("DELETE", $data);
